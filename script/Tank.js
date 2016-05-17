7e44eb8b927b930abaed4ec3f934332a17f00cec
Than Duc Thang <Than Duc Thang>
2016-05-17 21:40:25 +0700

    [ Update ] Tank can now shoot single bullet  - Bug : Bullets are shooted from the tank's wheel ._.

--- script/Tank.js
+++ script/Tank.js
@@ -8,7 +8,7 @@ class Tank {
         this.speedX = 0;
         this.speedY = 0;
         this.sprite = new Image();
-        this.sprite.src = "images/tank_basic_down_c0_t1_f.png";
+        this.sprite.src = "images/tank_basic_up_c0_t1_f.png";
         this.spriteUp = new Image();
         this.spriteUp.src = "images/tank_basic_up_c0_t1_f.png";
         this.spriteDown = new Image();
@@ -24,37 +24,34 @@ class Tank {
         this.x += this.speedX;
         this.y += this.speedY;
     }
-
     draw(context) {
         context.drawImage(this.sprite, this.x, this.y);
-
     }
-
     move(direction) {
         switch (direction) {
             case 1://Move up
                 this.speedY = -3;
                 this.speedX = 0;
                 this.sprite = this.spriteUp;
-                this.direction = 1;
+                this.direction = direction;
                 break;
             case 2://Move down
                 this.speedY = 3;
                 this.speedX = 0;
                 this.sprite = this.spriteDown;
-                this.direction = 2;
+                this.direction = direction;
                 break;
             case 3://Move left
                 this.speedX = -3;
                 this.speedY = 0;
                 this.sprite = this.spriteLeft;
-                this.direction = 3;
+                this.direction = direction;
                 break;
             case 4://Move right
                 this.speedX = 3;
                 this.speedY = 0;
                 this.sprite = this.spriteRight;
-                this.direction = 4;
+                this.direction = direction;
                 break;
         }
     }
--- script/game.js
+++ script/game.js
@@ -18,43 +18,63 @@ window.onload = function()
     }, 17);
 };
 var player;
+var playerBullet;
 function gameStart()
 {
 	//Tank is as known as the blueprint for building 'player', stored in Tank.js
+    //Bullet                                         'playerBullet', stored in Bullet.js
     player = new Tank(100,100);
+    playerBullet = new Bullet(0,0,1);
 }
 function gameUpdate()
 {
     player.update();
+    playerBullet.update();
 }
 function gameDrawer(context)
 {
-    context.fillStyle = "#C8BBBE";
+    context.fillStyle = "999999";
     context.fillRect(0,0,window.innerWidth,window.innerHeight);
 	player.draw(context);
+    playerBullet.draw(context);
 }
 window.onkeydown = function(e)
 {
     switch (e.keyCode)
     {
-
+        case 87 ://W button, Go Up
+            player.move(1);
+            break;
+        case 83 ://S button, Go Down
+            player.move(2);
+            break;
         case 65 ://A button, Go Left
             player.move(3);
             break;
         case 68 ://D button, Go Right
             player.move(4);
             break;
-        case 83 ://S button, Go Down
-            player.move(2);
-            break;
-        case 87 ://W button, Go Up
-            player.move(1);
+        case 32 ://Space Bar, Shoot
+            playerBullet.x = player.x;
+            playerBullet.y = player.y;
+            playerBullet.move(player.direction);
             break;
     }
-
 };
 window.onkeyup = function (e) {
     switch (e.keyCode){
+        case 87 ://W - Go Up
+            if(player.speedY < 0){
+                player.speedY = 0;
+                player.sprite = player.spriteUp;
+            }
+            break;  
+        case 83 :// S - Go Down
+            if(player.speedY > 0){
+                player.speedY = 0;
+                player.sprite = player.spriteDown;
+            }
+            break;
         case 65 :// A - Go Left
             if(player.speedX < 0){
                 player.speedX = 0;
@@ -67,17 +87,8 @@ window.onkeyup = function (e) {
                 player.sprite = player.spriteRight;
             }
             break;
-        case 83 :// S - Go Down
-            if(player.speedY > 0){
-                player.speedY = 0;
-                player.sprite = player.spriteDown;
-            }
-            break;
-        case 87 ://W - Go Up
-            if(player.speedY < 0){
-                player.speedY = 0;
-                player.sprite = player.spriteUp;
-            }
-            break;
+
     }
 };
+
+
